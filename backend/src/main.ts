a848dba4e06df764ef145eea7eb648c45a43261e
Michael V <mvanderlyn27@gmail.com>
2021-11-08 01:49:18 -0500

    added temp cocde

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -26,5 +26,9 @@ interface Loc {
 }
 
 function calcGoldenHour(loc: Loc, time: Date){
-
+  const twopi = 2 * Math.PI;
+  const deg2rad = Math.PI / 180;
+  const month_days = [31,28,31,30,31,30,31,31,30,31,30];
+  let day = month_days[time.getMonth()];
+  let leapdays = time.getFullYear() % 4 == 0 
 }
