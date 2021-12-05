acc7e6ba93496928e9af4ff127f491f1e38b5452
Michael V <mvanderlyn27@gmail.com>
2021-12-05 03:55:47 -0500

    added diff tyelrs

--- frontend/src/components/MapHolder.tsx
+++ frontend/src/components/MapHolder.tsx
@@ -1,9 +1,12 @@
 import { Map, Marker } from "pigeon-maps"
 
+function satMapTiler(x:number,y:number,z:number){return `https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/${z}/${y}/${x}`};
+function topoMapTiler(x:number,y:number,z:number){return `https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/${z}/${y}/${x}`};
+
 const MapHolder = () => {
 
     return (
-    <Map height={300} defaultCenter={[50.879, 4.6997]} defaultZoom={11}>
+    <Map provider = {topoMapTiler} height={1000} defaultCenter={[50.879, 4.6997]} defaultZoom={11}>
       <Marker width={50} anchor={[50.879, 4.6997]} />
     </Map>
     );
