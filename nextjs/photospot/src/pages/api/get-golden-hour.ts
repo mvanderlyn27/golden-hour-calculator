7cf4c2345bea983279acb8bc03812c1a3f684f74
michaelvanderlyn <mvanderlyn27@gmail.com>
2023-02-09 14:54:40 -0500

    added api functionality

--- nextjs/photospot/src/pages/api/get-golden-hour.ts
+++ nextjs/photospot/src/pages/api/get-golden-hour.ts
@@ -1,17 +1,27 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+let SunCalc = require('suncalc3');
 
 type Data = {
-  name?: string,
-  error?: string
+    goldenHourDawnStart?: string,
+    goldenHourDawnEnd?: string,
+    goldenHourDuskStart?: string,
+    goldenHourDuskEnd?: string,
+    error?: string
 }
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-    if(req.method === "POST"){
-
+    if(req.method === "PUT"){
+        //calc based on date/lat/lng and return in json
+        let date = req.body.date;
+        let lat = req.body.lat;
+        let long = req.body.long;
+        console.log(date,lat,long);
+        let solarTimes = SunCalc.getSunTimes(date, lat,long,0, false, true);
+        res.send({goldenHourDawnStart: solarTimes.goldenHourDawnStart, goldenHourDawnEnd: solarTimes.goldenHourDawnEnd, goldenHourDuskStart:solarTimes.goldenHourDuskStart, goldenHourDuskEnd: solarTimes.goldenHourDuskEnd})
     }
     else{
         res.status(400).send({ error: 'Need to use POST' });
